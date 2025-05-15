ed7c8108e89d2bd96a8f8ca61de5c31997eb0bb8
Spencer Seebald <sseebald@gmail.com>
2025-05-14 17:12:21 -0700

    Updating rebuild script with docker build args, updating dockerfiles with local versions

--- rebuild_images.sh
+++ rebuild_images.sh
@@ -24,7 +24,7 @@ echo "Building rebased Ping Identity containers using Chainguard images..."
 for product in "${PRODUCTS[@]}"; do
   if [ -d "$product" ]; then
     echo "Building $product..."
-    docker build -t "${product}-rebased:local" "./$product"
+    docker build --build-arg REGISTRY=localhost --build-arg GIT_TAG=latest --build-arg ARCH=amd64 -t "${product}-rebased:local" "./$product"
   else
     echo "Directory $product not found, skipping..."
   fi
